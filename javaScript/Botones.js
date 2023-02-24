b3bfe4c8ea09ceb92815bfe131411df95fa7d0f2
joburbanop <joburbanop589@gmail.com>
2023-02-24 12:58:13 -0500

    modificacion de algunos botones y el boton dar informacion y agendar cita

---
+++ javaScript/Botones.js
@@ -0,0 +1,39 @@
+const infoBtn = document.querySelector('#MasinformacionBTN1');
+        const infoBtn2 = document.querySelector('#MasinformacionBTN2');
+        const infoBtn3 = document.querySelector('#MasinformacionBTN3');
+        infoBtn.addEventListener('click', toggleInfo);
+        infoBtn2.addEventListener('click',toggleInfo2);
+        infoBtn3.addEventListener('click',toggleInfo3);
+
+        function toggleInfo() {
+          if (infoBtn.textContent === 'Más Información' ) {
+            infoBtn.textContent = 'Menos Información';
+            //console.log(infoBtn.textContent+'aqui arriba el if');
+          } else {
+            infoBtn.textContent = 'Más Información';
+            console.log(infoBtn.textContent);
+          }
+
+        }
+
+        function toggleInfo2() {
+          if (infoBtn2.textContent === 'Más Información' ) {
+            infoBtn2.textContent = 'Menos Información';
+            //console.log(infoBtn2.textContent+'aqui arriba el if');
+          } else {
+            infoBtn2.textContent = 'Más Información';
+            console.log(infoBtn2.textContent);
+          }
+
+        }
+
+        function toggleInfo3() {
+          if (infoBtn3.textContent === 'Más Información' ) {
+            infoBtn3.textContent = 'Menos Información';
+            //console.log(infoBtn3.textContent+'aqui arriba el if');
+          } else {
+            infoBtn3.textContent = 'Más Información';
+            console.log(infoBtn3.textContent);
+          }
+
+        }
