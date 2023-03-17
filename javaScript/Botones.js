26c222274b3d50306f0405ceaba2751fde64e90b
joburbanop <joburbanop589@gmail.com>
2023-03-17 12:23:03 -0500

    iconos incrocporados y correcion de erores

--- javaScript/Botones.js
+++ javaScript/Botones.js
@@ -1,9 +1,11 @@
-const infoBtn = document.querySelector('#MasinformacionBTN1');
+        const infoBtn = document.querySelector('#MasinformacionBTN1');
         const infoBtn2 = document.querySelector('#MasinformacionBTN2');
         const infoBtn3 = document.querySelector('#MasinformacionBTN3');
+        const infoBtn4 = document.querySelector('#MasinformacionBTN4');
         infoBtn.addEventListener('click', toggleInfo);
         infoBtn2.addEventListener('click',toggleInfo2);
         infoBtn3.addEventListener('click',toggleInfo3);
+        infoBtn4.addEventListener('click',toggleInfo4);
 
         function toggleInfo() {
           if (infoBtn.textContent === 'Más Información' ) {
@@ -33,6 +35,16 @@ const infoBtn = document.querySelector('#MasinformacionBTN1');
             //console.log(infoBtn3.textContent+'aqui arriba el if');
           } else {
             infoBtn3.textContent = 'Más Información';
+            //console.log(infoBtn3.textContent);
+          }
+
+        }
+        function toggleInfo3() {
+          if (infoBtn4.textContent === 'Más Información' ) {
+            infoBtn4.textContent = 'Menos Información';
+            //console.log(infoBtn3.textContent+'aqui arriba el if');
+          } else {
+            infoBtn4.textContent = 'Más Información';
             //console.log(infoBtn3.textContent);
           }
 
